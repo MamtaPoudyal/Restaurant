a062411e15ba0f85f61793c312c913c9ad57650b
Mamta Poudyal <lennoxlen46@gmail.com>
2025-04-09 22:31:37 +0530

    Explore-Menu Updated

--- frontend/src/components/FoodDisplay/FoddDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoddDisplay.jsx
@@ -3,7 +3,7 @@ import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 import './FoodDisplay.css'
 
-const FoddDisplay = (category) => {
+const FoddDisplay = ({category}) => {
 
     const {food_list} = useContext(StoreContext)
   return (
@@ -11,11 +11,11 @@ const FoddDisplay = (category) => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
+              if(category==="All" || category===item.category) {
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-
+              }
             })}
         </div>
-      
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,9 +3,8 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoddDisplay from '../../components/FoodDisplay/FoddDisplay';
-function Home() {
-
-    const[category,setCategory] = useState("All");
+const Home=() => {
+  const[category,setCategory] = useState("All");
   return (
     <div>
       <Header/>
