962149922337a5bcd7b0fc4f87685c9ce4d6267b
Mamta Poudyal <lennoxlen46@gmail.com>
2025-04-10 11:57:26 +0530

    Responsive

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoddDisplay from '../../components/FoodDisplay/FoddDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 const Home=() => {
   const[category,setCategory] = useState("All");
   return (
@@ -10,6 +11,7 @@ const Home=() => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoddDisplay category={category} />
+      <AppDownload/>
     
     </div>
   )
