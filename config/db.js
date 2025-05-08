a0f24b555ea69128148d96ef5ef9c52fae1bdb81
Shova Dulal <shova.23@msu.edu.in>
2025-05-08 12:02:19 +0530

    Removed incorrectly pushed files

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
- export const connectDB = async () => {
-   await mongoose.connect('mongodb+srv://shova:shovadulal@cluster0.q9ualiv.mongodb.net/Restaurant').then(()=>console.log("DB Connected"));
-}
